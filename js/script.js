82fdc6a0575e724d47a90ecd3d9b50d4bbd62e33
aplctc <118953276+aplctc@users.noreply.github.com>
2025-06-25 00:03:55 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 const questions = [
-  { text: "What is your name?", answers: ["Sakil", "your", "your name", "What is your name"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "Identify the fruit from the image below", image: " img/Image-1.jpeg", answers: ["কনক্রিট মিক্সার", "ভাইব্রেটর মেশিন", "রড বাঁকানোর মেশিন", "রড কাটার মেশিন"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "Identify the landmark", image: " img/Image-2.jpeg", answers: ["কনক্রিট মিক্সার", "ভাইব্রেটর মেশিন", "রড বাঁকানোর মেশিন", "জ্যাক হ্যামার"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "What is your hobby?", answers: ["Reading", "Writing", "Coding", "Gaming"] },
-  { text: "What is your name?", answers: ["Sakil", "your", "your name", "What is your name"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "Identify the fruit from the image below", image: " img/Image-3.jpeg", answers: ["কনক্রিট মিক্সার", "জ্যাক হ্যামার", "টোটাল স্টেশন/লেভেলিং যন্ত্র", "রড কাটার মেশিন"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "Identify the landmark", image: " img/Image-4.jpeg", answers: ["জ্যাক হ্যামার", " লেভেলিং যন্ত্র", "রড বাঁকানোর মেশিন", "রড কাটার মেশিন"] },
-  { text: "What is your favorite color?", answers: ["Red", "Blue", "Green", "Yellow"] },
-  { text: "What is your hobby?", answers: ["Reading", "Writing", "Coding", "Gaming"] }
+  { text: "Q: সিমেন্টের প্রধান উপাদান কী ?", answers: ["A) সিলিকা", "B) ক্যালসিয়াম", "C) লোহা", "D) বালি  "] },
+  { text: "Q: প্লাস্টারিংয়ের জন্য কোন মিক্স অনুপাতে ব্যবহার হয়?", answers: ["A) 1:6", "B) 1:3 ", "C) 1:4", "D) 1:5 "] },
+  { text: "Q: এটির নাম কি ?", image: " img/Image-1.jpeg", answers: ["কনক্রিট মিক্সার", "ভাইব্রেটর মেশিন", "রড বাঁকানোর মেশিন", "রড কাটার মেশিন"] },
+  { text: "Q: একটা ইটের স্ট্যান্ডার্ড সাইজ কত?", answers: ["A) 10x5x3 ইঞ্চি ", "B) 9x4.5x3 ইঞ্চি", "C) 8x5x2.5 ইঞ্চি", "D) 9x4x4 ইঞ্চি  "] },
+  { text: "Q: রেশিও 1:2:4 মানে কী?", answers: ["A) পানি:সিমেন্ট:বালি ", "B) সিমেন্ট:বালি:স্টোন", "C) সিমেন্ট:বালি:চিপস ", "D) বালি:সিমেন্ট:চুন "] },
+  { text: "Q: নিম্নের চিত্রটির নাম কি?", image: " img/Image-2.jpeg", answers: ["কনক্রিট মিক্সার", "ভাইব্রেটর মেশিন", "রড বাঁকানোর মেশিন", "জ্যাক হ্যামার"] },
+  { text: "Q: কনক্রিট সেট হতে কত সময় লাগে (প্রাথমিক)?", answers: ["A) ২ ঘণ্টা ", "B) ৩০ মিনিট  ", "C) ৬ ঘণ্টা ", "D) ১ ঘণ্টা "] },
+  { text: "Q: ইটের জোড়ায় কোন মেটেরিয়াল ব্যবহার হয়? ", answers: ["A) পানি", "B) চুন", "C) মিক্সড সিমেন্ট", "D) মর্টার "] },
+  { text: "Q: ১ ব্যাগ সিমেন্টে কত কেজি থাকে?", answers: ["A) ৪০ কেজি", "B) ৪৫ কেজি", "C) ৫০ কেজি", "D) ৫৫ কেজি"] },
+  { text: "Q: ছাদ ঢালার সময় সবচেয়ে জরুরি কী?", answers: ["A) রডের রং ", "B) কাঠের গুণ ", "C) লেবেলিং", "D) ইটের মান"] },
+  { text: "Q: এটির নাম কি ?", image: " img/Image-3.jpeg", answers: ["কনক্রিট মিক্সার", "জ্যাক হ্যামার", "টোটাল স্টেশন/লেভেলিং যন্ত্র", "রড কাটার মেশিন"] },
+  { text: "Q: ঢালাই করার আগে যে কাঠামো তৈরি করা হয় তার কি?", answers: ["A) মাটি", "B) প্লাস্টার ", "C) শাটারিং", "D) রড "] },
+  { text: "Q: নিম্নের চিত্রটির নাম কি?", image: " img/Image-4.jpeg", answers: ["জ্যাক হ্যামার", " লেভেলিং যন্ত্র", "রড বাঁকানোর মেশিন", "রড কাটার মেশিন"] },
+  { text: "Q: ছাদ ঢালার পর কতদিন পানি দিতে হয়?", answers: ["A) ৩ দিন ", "B) ৫ দিন", "C) ৭ দিন", "D) ১৪ দিন"] },
+  { text: "Q: প্লাম্ব বব কী মাপে/ সল?", answers: ["A) আড়াআড়ি সরলতা ", "B) খাড়াভাবে সোজা", "C) উচ্চতা", "D) প্রস্থ "] }
 ];
 
 let currentQuestion = 0;
